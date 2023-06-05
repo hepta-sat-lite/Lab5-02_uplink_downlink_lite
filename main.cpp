cacb1264fe45ea914f943ec9867552ef2cf4d85e
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-05 11:09:15 +0000

    commit65

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+#include "HEPTA_COM.h"
+HEPTA_COM com(PA_9,PA_10,9600);
+Serial sat(USBTX,USBRX,9600);
+int main()
+{
+    sat.printf("Xbee Uplink Ok Mode\r\n");
+    int rcmd=0,cmdflag=0;
+    for(int i=0;i<10;i++){
+        com.xbee_receive(&rcmd,&cmdflag);
+        com.printf("num=%d\r\n",i);
+        if(cmdflag==1){
+            if(rcmd=='a'){
+                sat.printf("rcmd=%c,cmdflag=%d\r\n",rcmd,cmdflag);
+                com.printf("Hepta-Sat Lite Uplink Ok\r\n");
+            }
+            com.initialize();
+        }
+        wait_ms(1000);
+    }
 }
